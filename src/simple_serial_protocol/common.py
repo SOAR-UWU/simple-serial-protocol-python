60999928c88497956e7a917a5a672c688f130ab3
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-01-23 11:35:29 +0800

    fix: add typing_extensions for TypeAlias support for python<3.10

--- src/simple_serial_protocol/common.py
+++ src/simple_serial_protocol/common.py
@@ -1,4 +1,9 @@
-from typing import Callable, TypeAlias
+from typing import Callable
+
+try:
+    from typing import TypeAlias
+except ImportError:
+    from typing_extensions import TypeAlias
 
 Byte: TypeAlias = int
 CommandCallback: TypeAlias = Callable[..., None]
